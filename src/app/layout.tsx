b6ff385c4a6ccd5c9e9f0e5bfe5b6e87252ef321
0xTani <91439831+0xTani@users.noreply.github.com>
2024-02-17 19:07:44 +0400

    before modif

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,10 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 
+import { WagmiProvider } from 'wagmi'
+import { config } from '../../config'
+
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -15,8 +19,10 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <WagmiProvider config={config}>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </WagmiProvider>
   )
 }
